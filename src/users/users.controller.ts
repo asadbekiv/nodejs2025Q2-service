9110f2572c197daad8fb8dbeb27255935bad015f
Asadbek <ibodullaev.asadbek@mail.ru>
2025-05-29 14:32:46 +0500

    feat: users service finish

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,58 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  HttpCode,
+  Param,
+  Body,
+  ParseUUIDPipe,
+  Put,
+  Delete,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
+import { UpdateUserDto } from './dto/update-user.dto';
 
-@Controller('users')
-export class UsersController {}
+@Controller('user')
+export class UsersController {
+  constructor(private userseService: UsersService) {}
+
+  @Get()
+  async findAll() {
+    return await this.userseService.getAllUsers();
+  }
+  @Get(':id')
+  async findOne(@Param('id', new ParseUUIDPipe()) id: string) {
+    const user = await this.userseService.getUserById(id);
+    return user;
+  }
+
+  @Post()
+  async create(@Body() createUserDto: CreateUserDto) {
+    const user = await this.userseService.createUser(
+      createUserDto.login,
+      createUserDto.password,
+    );
+
+    return user;
+  }
+
+  @Put(':id')
+  async update(
+    @Body() updateUserDto: UpdateUserDto,
+    @Param('id', new ParseUUIDPipe()) id: string,
+  ) {
+    const updatedUser = await this.userseService.updatePassword(
+      id,
+      updateUserDto.oldPassword,
+      updateUserDto.newPassword,
+    );
+
+    return updatedUser;
+  }
+  @HttpCode(204)
+  @Delete(':id')
+  async delete(@Param('id', new ParseUUIDPipe()) id: string) {
+    await this.userseService.delete(id);
+  }
+}
